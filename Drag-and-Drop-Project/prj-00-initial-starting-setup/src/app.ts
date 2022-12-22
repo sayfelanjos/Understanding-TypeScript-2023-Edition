d3cd9d23fcd61ba0c7f6976ec2c2ff0da2c7b1c6
sayfelanjos <saymonfelipe@hotmail.com>
2022-12-22 17:03:03 -0300

    Interacting with DOM Elements

--- Drag-and-Drop-Project/prj-00-initial-starting-setup/src/app.ts
+++ Drag-and-Drop-Project/prj-00-initial-starting-setup/src/app.ts
@@ -2,13 +2,27 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopletitleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement =  <HTMLTemplateElement>document.getElementById("project-input");
     this.hostElement = <HTMLDivElement>document.getElementById(("app"));
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+    this.titleInputElement = <HTMLInputElement>this.element.querySelector("#title");
+    this.descriptionInputElement = <HTMLInputElement>this.element.querySelector("#description");
+    this.peopletitleInputElement = <HTMLInputElement>this.element.querySelector("#people");
     this.attach();
+  }
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
